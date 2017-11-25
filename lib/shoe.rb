696c9ea0944f8afe2f8d748ee3fd6c4c8529b77d
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-25 22:11:42 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,9 @@
 class Shoe
-  attr_accessor :brand 
+  attr_accessor :brand
   attr_accessor :color 
-  attr_accessor :size 
-  attr_accessor :material 
+  attr_accessor :size
+  attr_accessor :material
   attr_accessor :condition
 
-  
+
 end
